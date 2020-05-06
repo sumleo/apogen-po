8dfdeb3d9120359b2ecaf8af71db34a555428538
louiisliu <11610522@mail.sustech.edu.cn>
2020-05-07 02:36:03 +0800

    add retroboard

--- retroboard/ClassUnderTestApogen.java
+++ retroboard/ClassUnderTestApogen.java
@@ -16,31 +16,18 @@ public class ClassUnderTestApogen {
 		// start driver and browser
 		WebDriver driver = new DriverProvider().getActiveDriver();
 
-		this.currentPage = new Index(driver);
+		this.currentPage = new po.Index(driver);
 	}
 
-	// PO Name: Index
-	public void goToState2Index() {
-		if (this.currentPage instanceof po.Index) {
-			Index page = (Index) this.currentPage;
+	// PO Name: LoginPage
+	public void goToState2LoginPage() {
+		if (this.currentPage instanceof main.LoginPage) {
+			main.LoginPage page = (main.LoginPage) this.currentPage;
 			page.button_Letsstart.click();
-			this.currentPage = new State2(page.driver);
+			this.currentPage = new main.State2(page.driver);
 		} else {
 			throw new NotInTheRightPageObjectException(
-					"goToState2Index: expected po.Index, found "
-							+ this.currentPage.getClass().getSimpleName());
-		}
-	}
-
-	// PO Name: State2
-	public void goToIndexState2() {
-		if (this.currentPage instanceof po.State2) {
-			State2 page = (State2) this.currentPage;
-			page.a_Retrospectedagoodwayo.click();
-			this.currentPage = new Index(page.driver);
-		} else {
-			throw new NotInTheRightPageObjectException(
-					"goToIndexState2: expected po.State2, found "
+					"goToState2LoginPage: expected main.LoginPage, found "
 							+ this.currentPage.getClass().getSimpleName());
 		}
 	}
